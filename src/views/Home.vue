cb01a82b9eb408850afa49e842fb62fa3dd42f2b
Koichi Miyamoto <m5813k@gmail.com>
2018-12-24 12:44:04 +0900

    table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,38 @@
 <template>
-  <div class="home">
+  <div class="home container">
+    <b-table :data="arr" :columns="columns" :mobile-cards="false"></b-table>
   </div>
 </template>
 
 <script>
+//prettier-ignore
+const arr = [
+  { 'id': 1, 'first_name': 'Jesse',    },
+  { 'id': 2, 'first_name': 'John',    },
+  { 'id': 3, 'first_name': 'Tina',    },
+  { 'id': 4, 'first_name': 'Clarence',    },
+  { 'id': 5, 'first_name': 'Anne',    }
+]
+const columns = [
+  {
+    field: "id",
+    label: "ID",
+    width: "40",
+    numeric: true
+  },
+  {
+    field: "first_name",
+    label: "First Name"
+  }
+];
 export default {
   name: "home",
-  components: {}
+  components: {},
+  data() {
+    return {
+      arr,
+      columns
+    };
+  }
 };
 </script>
