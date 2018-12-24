e6d4b1520e91f3696a7e26aab65b7cf26026d82b
Koichi Miyamoto <m5813k@gmail.com>
2018-12-24 11:55:53 +0900

    headerきれいにした

--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -1,10 +1,17 @@
 <template>
-  <header>
-  <div class="content-root" >
-    <div class="menu-button"></div>
-    <h1>管理者画面</h1>
-  </div>
-  </header>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" >
+        <h1>管理者画面</h1>
+      </a>
+
+      <a role="button" class="navbar-burger" >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  </nav>
 </template>
 <script>
 </script>
