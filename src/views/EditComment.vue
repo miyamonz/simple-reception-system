bfb77ea092f47ca826f21b7e033689bd6c9c6c6b
Koichi Miyamoto <m5813k@gmail.com>
2018-12-24 20:34:58 +0900

    edit coment vue

--- src/views/EditComment.vue
+++ src/views/EditComment.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="edit-comment">
-    <section>
-    <h2 class=""></h2>
-    <b-field label="コメントの編集">
-      <b-input type="textarea"></b-input>
-    </b-field>
-    </section>
+  <div id="edit-comment" >
+    <div class="column">
+      <b-field label="コメントの編集">
+        <b-input type="textarea"></b-input>
+      </b-field>
+      <div class="buttons is-right">
+        <button class="button is-primary"
+          :class=" sending && 'is-loading'"
+          >送信</button>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return { sending: false };
+  }
+};
+</script>
