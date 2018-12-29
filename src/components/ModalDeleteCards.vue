3f270dd9f7c73c3624a4f92970fb1ff6069035ab
Koichi Miyamoto <m5813k@gmail.com>
2018-12-29 13:43:50 +0900

    delete cardするmodal

---
+++ src/components/ModalDeleteCards.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="modal-card">
+    <div class="modal-card-head">
+      カードの消去
+    </div>
+    <div class="modal-card-body">
+      本当に消去しますか？
+    </div>
+    <footer class="modal-card-foot">
+      <button class="button" 
+         @click="$parent.close()">キャンセル</button>
+      <button class="button is-danger">消去の実行</button>
+    </footer>
+  </div>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>設定</h1>
+    <button class="button is-primary" @click="isModalActive = true">
+      カードの消去
+    </button>
+    <b-modal :active.sync="isModalActive" has-modal-card>
+      <ModalDeleteCards />
+    </b-modal>
   </div>
 </template>
+<script>
+import ModalDeleteCards from "@/components/ModalDeleteCards.vue";
+export default {
+  components: { ModalDeleteCards },
+  data() {
+    return { isModalActive: false };
+  }
+};
+</script>
