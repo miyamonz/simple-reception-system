3410c1a08c95195242b7b716ce2dba0142f4df58
Koichi Miyamoto <m5813k@gmail.com>
2018-12-30 11:25:00 +0900

    vueのstyleにimportするようにした

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+const path = require("path");
 module.exports = {
+  chainWebpack: config => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach(type =>
+      addStyleResource(config.module.rule("scss").oneOf(type))
+    );
+  },
   devServer: {
     host: "0.0.0.0",
     proxy: {
@@ -9,3 +16,12 @@ module.exports = {
     }
   }
 };
+
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [path.resolve(__dirname, "./src/main.scss")]
+    });
+}
