30f83c5b0d2d2e7182eff4349009cb9137053415
Koichi Miyamoto <m5813k@gmail.com>
2018-12-31 14:08:14 +0900

    呼び出し中アイコンの座標をresize時に再計算するようにした

--- src/components/CallingTag.vue
+++ src/components/CallingTag.vue
@@ -8,17 +8,37 @@
   </div>
 </template>
 <script>
+import onresize from "@/debounceResize.js";
 export default {
+  mounted() {
+    onresize(this.setLeft);
+    setTimeout(this.setLeft, 10);
+  },
   computed: {
-    left() {
-      return 100 + "px";
-    },
     top() {
+      this.setLeft();
       const i = this.$store.getters.callingIndex;
       //高さ .rowに指定したheight
       //3rem 本来はcss variableにしてjsで動的に読むとか
       return i * 3 + "rem";
     }
+  },
+  methods: {
+    setLeft() {
+      this.left = this.getLeft();
+    },
+    getLeft() {
+      const element = document.querySelector(".cell-slot");
+      if (!element) return;
+      const rect = element.getBoundingClientRect();
+      if (!rect) return;
+      return rect.left + "px";
+    }
+  },
+  data() {
+    return {
+      left: 0
+    };
   }
 };
 </script>
