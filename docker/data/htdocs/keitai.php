d820e736b219eeeeb94c7178fb7ca8d45216c567
Koichi Miyamoto <m5813k@gmail.com>
2018-12-31 15:49:00 +0900

    keitaiの表示させるようにした

--- docker/data/htdocs/keitai.php
+++ docker/data/htdocs/keitai.php
@@ -21,6 +21,10 @@ $end_text = '本日の診療は<font color="red">終了</font>しました<br>';
     padding-bottom: 15px;
     }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js"></script>
+    <script>
+    let state = <?=  file_get_contents("./api/data.json" ) ?>;
+    </script>
   </head>
   <body text="#000000" link="blue" vlink="blue" bgcolor="#F7F7D7">
     <div align="center">
@@ -46,7 +50,10 @@ $end_text = '本日の診療は<font color="red">終了</font>しました<br>';
 
       <section style="color: maroon">
         <header>診察状況</header>
-        <?=  file_get_contents("./api/data.json" ) ?>
+        <div id="app">
+          呼び出し中: {{state.calling}}<br>
+          コメント:{{state.comment}}
+        </div>
       </section>
       <hr>
       <a href="keitai.php">〔最新の状況に更新〕</a>
@@ -55,5 +62,13 @@ $end_text = '本日の診療は<font color="red">終了</font>しました<br>';
     このWEBページは受付を行うものではありません。<br>
     受付は来院の上お願いします。
     <hr>
+    <script>
+    console.log(state)
+    const vue = new Vue({el:"#app",data: {state}})
+    setInterval( () =>  {
+      const ts = Math.floor(new Date().getTime() / 1000);
+      fetch(`./api/data.json?ts=${ts}`).then( res =>  res.json() ).then(json => vue.state = {...json})
+    }, 5000);
+    </script>
   </body>
 </html>
