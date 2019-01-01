c0518fee64e007b5b543cb0f164bcf3d9640c960
Koichi Miyamoto <m5813k@gmail.com>
2019-01-01 21:35:19 +0900

    debounceかけた

--- src/api.js
+++ src/api.js
@@ -1,9 +1,21 @@
 import axios from "axios";
-export const pushState = async state => {
+
+const debounce = (fn, ms = 500) => {
+  let timer = 0;
+  return param => {
+    if (timer > 0) clearTimeout(timer);
+    timer = setTimeout(() => fn(param), ms);
+  };
+};
+
+const _pushState = async state => {
   const res = await axios.post("api/", state);
   console.log(res.statusText, res.data);
   return res.data;
 };
+
+export const pushState = debounce(_pushState);
+
 export const pullState = async () => {
   const ts = Math.floor(new Date().getTime() / 1000);
   return axios.get(`api/?timestamp=${ts}`).then(res => res.data);
