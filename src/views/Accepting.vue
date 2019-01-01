3cb168894fb00783c51800fbe966ef0d3161164f
Koichi Miyamoto <m5813k@gmail.com>
2019-01-01 15:07:48 +0900

    ボタンを用意

--- src/views/Accepting.vue
+++ src/views/Accepting.vue
@@ -2,12 +2,29 @@
   <div id="accepting" >
     <div class="column">
       <h2 class="title">診察開始と終了</h2>
+      {{accepting}}
+      <br>
+      <div>
+        <button class="button is-primary" :disabled=" accepting" @click="set(true)"> 開始する </button>
+        <br>
+        <br>
+        <button class="button is-primary" :disabled="!accepting" @click="set(false)"> 停止する </button>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  methods: {}
+  computed: {
+    accepting() {
+      return this.$store.state.accepting;
+    }
+  },
+  methods: {
+    set(b) {
+      this.$store.commit("setAccepting", b);
+    }
+  }
 };
 </script>
 <style>
