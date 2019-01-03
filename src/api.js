192eb75e8b66910d8a8950a1cbb883d0b2c9ff13
Koichi Miyamoto <m5813k@gmail.com>
2019-01-03 12:12:05 +0900

    VUE_APP_ENV

--- src/api.js
+++ src/api.js
@@ -12,12 +12,14 @@ const debounce = (fn, ms = 500) => {
     });
   };
 };
+const base =
+  process.env.VUE_APP_ENV === "development"
+    ? ""
+    : process.env.VUE_APP_API_BASE_URL;
 
 const _pushState = async state => {
   console.log("_pushState", state);
-  const res = await axios
-    .post(process.env.VUE_APP_API_BASE_URL + "api/", state)
-    .catch(console.error);
+  const res = await axios.post(base + "api/", state).catch(console.error);
   console.log(res.statusText, res.data);
   if (res.status === 200) {
     /*
@@ -34,7 +36,5 @@ export const pushState = debounce(_pushState);
 
 export const pullState = async () => {
   const ts = Math.floor(new Date().getTime() / 1000);
-  return axios
-    .get(process.env.VUE_APP_API_BASE_URL + `api/?timestamp=${ts}`)
-    .then(res => res.data);
+  return axios.get(base + `api/?timestamp=${ts}`).then(res => res.data);
 };
