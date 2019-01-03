747f5f67f384b701e7fb0ffbfcb7de5b823eae9d
Koichi Miyamoto <m5813k@gmail.com>
2019-01-03 15:51:25 +0900

    keitaiをstate参照して終了時の画面入れた

--- docker/data/htdocs/keitai.php
+++ docker/data/htdocs/keitai.php
@@ -4,8 +4,14 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-$end_text = '本日の診療は<font color="red">終了</font>しました<br>';
 
-$state = file_get_contents("./api/data.json" );
+$url = getenv('APP_ENV') === 'development' ? "./api/data.json" : "http://os3-373-19830.vs.sakura.ne.jp:8000/api/";
+$state = json_decode(file_get_contents($url));
+$accepting = $state->accepting;
+$call_num = $state->calling;
+
+$joukyou = "<span style='font-weight: bold; font-size:30px'>{$call_num}番</span>を呼び出し中です。<br> ";
+$end_text = '本日の診療は<font color="red">終了</font>しました<br>';
+if(!$accepting) $joukyou = $end_text;
 
 include "./keitai.html";
